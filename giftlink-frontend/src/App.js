e4f0d27f22a1f6c99514bc2491f19d933c1877b3
Sayyam Ahmed <sayyamabbasi802@gmail.com>
2025-02-05 19:33:37 +0500

    front end integrated

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 function App() {
-
   return (
-    <>
+        <>
         <Navbar/>
         <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
         </Routes>
         </>
   );
 }
-
 export default App;
